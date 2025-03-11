78ce08d18f89a033d54f5247bae8a654525522e7
Max Netyaga <max.netyaga@gmail.com>
2025-03-11 16:50:36 +0200

    Init

---
+++ list.py
@@ -0,0 +1,32 @@
+type char = str
+
+class List:
+    def __init__(self) -> None:
+        pass
+        
+    def length(self) -> int:
+        pass
+
+    def get(self, index: int) -> char:
+        pass
+
+    def append(self, element: char) -> None:
+        pass
+
+    def extend(self, elements: list) -> None:
+        pass
+
+    def delete(self, index: int) -> char:
+        pass
+
+    def deleteAll(self, element: char) -> None:
+        pass
+
+    def reverse(self, ) -> None:
+        pass
+
+    def findLast(self, element: char) -> int:
+        pass
+
+    def clear(self) -> None:
+        pass
