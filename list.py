d299ba76b96976b72f31856eeb273889311e3296
Max Netyaga <max.netyaga@gmail.com>
2025-03-11 17:56:02 +0200

    implemented list class using built-in list

--- list.py
+++ list.py
@@ -1,8 +1,16 @@
+from copy import deepcopy
+
+
 type char = str
 
 class List:
+    @staticmethod
+    def check_char(char):
+        if type(char) != str or len(char) != 1:
+            raise ValueError("Element must be a character")
+
     def __init__(self) -> None:
-        pass
+        self.data = []
         
     def length(self) -> int:
         '''Операція визначення довжини списку.
@@ -10,7 +18,7 @@ class List:
         Якщо список непорожній, то ця операція повинна повертати кількість елементів у списку.
         Якщо список порожній, то ця операція повинна повертати 0.
         '''
-        pass
+        return len(self.data)
 
     def get(self, index: int) -> char:
         '''Операція отримання елементу списку на довільній позиції.
@@ -18,11 +26,13 @@ class List:
         У випадку передачі некоректного значення позиції (наприклад, від’ємне число, або число, більше за індекс
         останнього елементу списку) метод повинен генерувати виключну ситуацію.
         '''
-        pass
+        return self.data[index]
 
     def append(self, element: char) -> None:
         '''Операцію додавання елементу в кінець списку.'''
-        pass
+        List.check_char(element)
+
+        self.data.append(element)
 
     def extend(self, elements: "List") -> None:
         '''Операцію розширення списку.
@@ -30,7 +40,7 @@ class List:
         Метод приймає інший список та додає до поточного списку усі елементи останнього.
         При цьому подальші зміни в другий список не повинні впливати на перший.
         '''
-        pass
+        self.data.extend(elements.data)
 
     def delete(self, index: int) -> char:
         '''Операцію видалення елементу зі списку на вказаній позиції.
@@ -40,7 +50,7 @@ class List:
         У випадку передачі некоректного значення позиції (наприклад, від’ємне число, або число, більше за індекс
         останнього елементу списку) метод повинен генерувати виключну ситуацію
         '''
-        pass
+        return self.data.pop(index)
 
     def deleteAll(self, element: char) -> None:
         '''Операцію видалення елементів зі списку за значенням.
@@ -48,14 +58,16 @@ class List:
         Метод видаляє зі списку усі елементи, які за значенням відповідають шуканому.
         У випадку передачі елемента, який у списку відсутній, жодні зміни до списку не застосовуються.
         '''
-        pass
+        List.check_char(element)
+
+        self.data = [x for x in self.data if x != element]
 
     def clone(self) -> "List":
         '''Операція копіювання списку.
 
         При виклику повинен створити копію поточного списку та повернути її.
         '''
-        pass
+        return deepcopy(self)
 
     def reverse(self) -> None:
         '''Операція обернення списку.
@@ -63,7 +75,7 @@ class List:
         Метод повинен змінити порядок елементів у поточному списку задом наперед.
         Елемент, що був останнім стане першим, передостаннім — другим, … а перший — останнім.
         '''
-        pass
+        self.data.reverse()
 
     def findFirst(self, element: char) -> int:
         '''Операція пошуку елемента за значенням з голови списку.
@@ -72,7 +84,12 @@ class List:
         Нумерація елементів списку починається з 0.
         У випадку відсутності шуканого елемента у списку, метод повертає -1
         '''
-        pass
+        List.check_char(element)
+        
+        try:
+            return self.data.index(element)
+        except ValueError:
+            return -1
 
     def findLast(self, element: char) -> int:
         '''Операція пошуку елемента за значенням з хвоста списку.
@@ -81,11 +98,16 @@ class List:
         Нумерація елементів списку починається з 0.
         У випадку відсутності шуканого елемента у списку, метод повертає -1.
         '''
-        pass
+        List.check_char(element)
+        
+        try:
+            return len(element) - 1 - self.data.index(element[::-1])
+        except ValueError:
+            return -1
 
     def clear(self) -> None:
         '''Операцію очищення списку.
 
         Метод видаляє усі елементи списку.
         '''
-        pass
+        self.data.clear()
