aab2bb6d3605d90fee4d18fe81c1dc4b37ff0862
Max Netyaga <max.netyaga@gmail.com>
2025-03-11 22:49:28 +0200

    rewrite as singly linked circular list

--- list.py
+++ list.py
@@ -1,27 +1,62 @@
-from copy import deepcopy
-
-
 type char = str
 
 
+def check_char(char):
+    if type(char) != str or len(char) != 1:
+        raise ValueError("Element must be a character")
+
+
+class _ListNode:
+    def __init__(self, value: char, next: "_ListNode | None" = None) -> None:
+        check_char(value)
+        self.value = value
+        self.next = next
+
+
 class List:
-    @staticmethod
-    def check_char(char):
-        if type(char) != str or len(char) != 1:
-            raise ValueError("Element must be a character")
-
     def __init__(self, *chars) -> None:
-        for char in chars:
-            List.check_char(char)
+        if not chars:
+            self.clear()
+            return
 
-        self._data = list(chars)
+        for char in chars:
+            check_char(char)
+
+        self._head = _ListNode(chars[0])
+        self._tail = self._head
+
+        cur_node = self._head
+        for char in chars[1:]:
+            cur_node.next = _ListNode(char)
+            cur_node = cur_node.next
+            self._tail = cur_node
+
+        # Cyclic list
+        self._tail.next = self._head
+
+        self._length = len(chars)
 
     def __str__(self) -> str:
-        return "".join(self._data)
+        if not self._head:
+            return ""
+
+        result = ""
+
+        cur_node = self._head
+        while True:
+            assert cur_node
+
+            result += cur_node.value
+
+            cur_node = cur_node.next
+            if cur_node is self._head:
+                break
+
+        return result
 
     def __eq__(self, value: "List") -> bool:
         return (isinstance(value, List)
-                and self._data == value._data)
+                and str(self) == str(value))
 
     def length(self) -> int:
         '''Операція визначення довжини списку.
@@ -29,7 +64,22 @@ class List:
         Якщо список непорожній, то ця операція повинна повертати кількість елементів у списку.
         Якщо список порожній, то ця операція повинна повертати 0.
         '''
-        return len(self._data)
+        return self._length
+
+    def _get_node(self, index: int) -> _ListNode:
+        if index < 0 or index >= self.length():
+            raise IndexError()
+
+        cur_node = self._head
+        cur_node_i = 0
+        while True:
+            assert cur_node
+
+            if cur_node_i == index:
+                return cur_node
+
+            cur_node = cur_node.next
+            cur_node_i += 1
 
     def get(self, index: int) -> char:
         '''Операція отримання елементу списку на довільній позиції.
@@ -37,16 +87,25 @@ class List:
         У випадку передачі некоректного значення позиції (наприклад, від’ємне число, або число, більше за індекс
         останнього елементу списку) метод повинен генерувати виключну ситуацію.
         '''
-        if index < 0:
-            raise IndexError()
-
-        return self._data[index]
+        return self._get_node(index).value
 
     def append(self, element: char) -> None:
         '''Операцію додавання елементу в кінець списку.'''
-        List.check_char(element)
+        check_char(element)
 
-        self._data.append(element)
+        if not self._head:
+            self._head = _ListNode(element)
+            self._tail = self._head
+            self._tail.next = self._head
+
+        else:
+            assert self._tail
+
+            self._tail.next = _ListNode(element)
+            self._tail = self._tail.next
+            self._tail.next = self._head
+
+        self._length += 1
 
     def extend(self, elements: "List") -> None:
         '''Операцію розширення списку.
@@ -54,7 +113,8 @@ class List:
         Метод приймає інший список та додає до поточного списку усі елементи останнього.
         При цьому подальші зміни в другий список не повинні впливати на перший.
         '''
-        self._data.extend(elements._data)
+        for i in range(elements.length()):
+            self.append(elements.get(i))
 
     def delete(self, index: int) -> char:
         '''Операцію видалення елементу зі списку на вказаній позиції.
@@ -64,10 +124,28 @@ class List:
         У випадку передачі некоректного значення позиції (наприклад, від’ємне число, або число, більше за індекс
         останнього елементу списку) метод повинен генерувати виключну ситуацію
         '''
-        if index < 0:
+        if index < 0 or index >= self.length():
             raise IndexError()
 
-        return self._data.pop(index)
+        assert self._tail
+
+        node_to_delete = self._get_node(index)
+        node_to_delete_prev = self._get_node(index-1) if index != 0 else None
+
+        if node_to_delete is self._head:
+            self._head = node_to_delete.next
+            self._tail.next = self._head
+        elif node_to_delete is self._tail:
+            assert node_to_delete_prev
+            node_to_delete_prev.next = self._head
+            self._tail = node_to_delete_prev
+        else:
+            assert node_to_delete_prev
+            node_to_delete_prev.next = node_to_delete.next
+
+        self._length -= 1
+
+        return node_to_delete.value
 
     def delete_all(self, element: char) -> None:
         '''Операцію видалення елементів зі списку за значенням.
@@ -75,16 +153,32 @@ class List:
         Метод видаляє зі списку усі елементи, які за значенням відповідають шуканому.
         У випадку передачі елемента, який у списку відсутній, жодні зміни до списку не застосовуються.
         '''
-        List.check_char(element)
+        check_char(element)
 
-        self._data = [x for x in self._data if x != element]
+        cur_node = self._head
+        cur_node_i = 0
+        while True:
+            assert cur_node
+
+            if cur_node.value == element:
+                self.delete(cur_node_i)
+                cur_node_i -= 1
+
+            cur_node = cur_node.next
+            cur_node_i += 1
+            if cur_node is self._head:
+                break
 
     def clone(self) -> "List":
         '''Операція копіювання списку.
 
         При виклику повинен створити копію поточного списку та повернути її.
         '''
-        return deepcopy(self)
+        clone = List()
+        for i in range(self.length()):
+            clone.append(self.get(i))
+
+        return clone
 
     def reverse(self) -> None:
         '''Операція обернення списку.
@@ -92,7 +186,26 @@ class List:
         Метод повинен змінити порядок елементів у поточному списку задом наперед.
         Елемент, що був останнім стане першим, передостаннім — другим, … а перший — останнім.
         '''
-        self._data.reverse()
+        if not self._head or not self._head.next:
+            return
+
+        prev_node = None
+        cur_node = self._head
+        first_node = self._head
+
+        while True:
+            assert cur_node
+
+            next_node = cur_node.next
+            cur_node.next = prev_node
+            prev_node = cur_node
+            cur_node = next_node
+
+            if cur_node == first_node:
+                break
+
+        first_node.next = prev_node
+        self._head = prev_node
 
     def find_first(self, element: char) -> int:
         '''Операція пошуку елемента за значенням з голови списку.
@@ -101,12 +214,13 @@ class List:
         Нумерація елементів списку починається з 0.
         У випадку відсутності шуканого елемента у списку, метод повертає -1
         '''
-        List.check_char(element)
+        check_char(element)
 
-        try:
-            return self._data.index(element)
-        except ValueError:
-            return -1
+        for i in range(self.length()):
+            if self.get(i) == element:
+                return i
+
+        return -1
 
     def find_last(self, element: char) -> int:
         '''Операція пошуку елемента за значенням з хвоста списку.
@@ -115,16 +229,20 @@ class List:
         Нумерація елементів списку починається з 0.
         У випадку відсутності шуканого елемента у списку, метод повертає -1.
         '''
-        List.check_char(element)
+        check_char(element)
 
-        try:
-            return len(self._data) - 1 - self._data.index(element[::-1])
-        except ValueError:
-            return -1
+        last_index = -1
+        for i in range(self.length()):
+            if self.get(i) == element:
+                last_index = i
+
+        return last_index
 
     def clear(self) -> None:
         '''Операцію очищення списку.
 
         Метод видаляє усі елементи списку.
         '''
-        self._data.clear()
+        self._head = None
+        self._tail = None
+        self._length = 0
